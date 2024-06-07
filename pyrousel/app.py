55e8678de04bd9bf6d14987be67b9461e624dd09
RealDanTheMan <dantheman.py@gmail.com>
2024-06-07 18:54:37 +0000

    Introduced application settings and exposed window dimensions

--- pyrousel/app.py
+++ pyrousel/app.py
@@ -1,9 +1,15 @@
+from dataclasses import dataclass
 from .appwindow import AppWindow
 
-def Run() -> None:
+@dataclass
+class ApplicationSettings:
+    window_width: int = 1024
+    window_height: int = 1024
+
+def Run(settings: ApplicationSettings = ApplicationSettings()) -> None:
     print('Running Pyrousel...')
-    app_window = AppWindow(1280, 1280)
-    app_window.Init()
+    app_window = AppWindow(settings.window_width, settings.window_height)
+    app_window.Init()    
     app_window.Run()
     print('Quitting Pyrousel')
 
